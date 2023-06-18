67ab630bf79a5ba7cb2d4d3d7ad709af6066e4a0
tokalopaidi1 <129062153+tokalopaidi1@users.noreply.github.com>
2023-06-18 11:07:50 +0300

    Update go.py

--- go.py
+++ go.py
@@ -18,7 +18,6 @@ def monte_carlo_simulation(n_runs, fund, n_investments, vc_failure_rate, vc_rang
         for _ in range(n_runs):
             portfolio_return = 0
 
-            # VC Deals
             vc_returns = np.zeros(vc_deals)
             vc_outcomes = np.random.random(vc_deals)
             vc_failures = vc_outcomes < vc_failure_rate
@@ -30,14 +29,13 @@ def monte_carlo_simulation(n_runs, fund, n_investments, vc_failure_rate, vc_rang
                 elif vc_range1[i]:
                     multiplier = np.random.uniform(2, 15)
                 else:
-                    power_law_dist = powerlaw(a=vc_power_law_exponent, scale=15.0)
-                    multiplier = max(power_law_dist.rvs(), 15.0)
+                    power_law_dist = powerlaw(a=vc_power_law_exponent, scale=15.0)  # change scale to 15 for x15-x200
+                    multiplier = power_law_dist.rvs()
 
                 investment = (fund / n_investments)
                 portfolio_return += investment * multiplier
                 vc_returns[i] = multiplier
 
-            # Growth Deals
             growth_returns = np.zeros(growth_deals)
             growth_outcomes = np.random.normal(growth_distribution_mean, growth_distribution_std, growth_deals)
             growth_failures = growth_outcomes < growth_failure_rate
@@ -55,16 +53,22 @@ def monte_carlo_simulation(n_runs, fund, n_investments, vc_failure_rate, vc_rang
                 portfolio_return += investment * multiplier
                 growth_returns[i] = multiplier
 
-            # Convert portfolio return to multiple of invested fund
-            portfolio_return_multiple = portfolio_return / fund
-            results.append({'growth_deals': growth_deals, 'portfolio_return_multiple': portfolio_return_multiple,
+            results.append({'growth_deals': growth_deals, 'portfolio_return': portfolio_return,
                             'vc_returns': vc_returns, 'growth_returns': growth_returns})
 
     df = pd.DataFrame(results)
 
     summary = df.groupby('growth_deals').agg(
-        mean_return=pd.NamedAgg(column='portfolio_return_multiple', aggfunc='mean'),
-        return_distribution=pd.NamedAgg(column='portfolio_return_multiple', aggfunc=lambda x: list(x))
+        mean_return=pd.NamedAgg(column='portfolio_return', aggfunc='mean'),
+        max_return=pd.NamedAgg(column='portfolio_return', aggfunc='max'),
+        min_return=pd.NamedAgg(column='portfolio_return', aggfunc='min'),
+        std_dev=pd.NamedAgg(column='portfolio_return', aggfunc='std'),
+        percentile_25=pd.NamedAgg(column='portfolio_return', aggfunc=lambda x: np.percentile(x, 25)),
+        median=pd.NamedAgg(column='portfolio_return', aggfunc='median'),
+        percentile_75=pd.NamedAgg(column='portfolio_return', aggfunc=lambda x: np.percentile(x, 75)),
+        prob_2x=pd.NamedAgg(column='portfolio_return', aggfunc=lambda x: np.mean(x >= 2 * fund)),
+        prob_3x=pd.NamedAgg(column='portfolio_return', aggfunc=lambda x: np.mean(x >= 3 * fund)),
+        prob_5x=pd.NamedAgg(column='portfolio_return', aggfunc=lambda x: np.mean(x >= 5 * fund))
     ).reset_index()
 
     return df, summary
@@ -74,7 +78,6 @@ def main():
 
     st.title('Monte Carlo Simulation App')
 
-    # Sidebar controls
     st.sidebar.title('Simulation Parameters')
     n_runs = st.sidebar.number_input('Number of Runs', value=1000, min_value=1)
     fund = st.sidebar.number_input('Initial Fund Amount', value=8000000, min_value=1)
@@ -89,7 +92,9 @@ def main():
     st.sidebar.title('Growth Deals')
     growth_failure_rate = st.sidebar.slider('Growth Percentage of Failure', 0.0, 1.0, 0.1, step=0.01)
     growth_range1_rate = st.sidebar.slider('Growth Percentage for 2x-7x', 0.0, 1.0, 0.7, step=0.01)
-    growth_range2_rate = st.sidebar.slider('Growth Percentage for 8x-20x', 0.0, 1.0, 0.2, step=0.01)
+    growth_range2_rate = st.sidebar.slider('Growth Percentage for 3x-20x', 0.0, 1.0, 0.2, step=0.01)
+
+    st.sidebar.title('Growth Deals Distribution')
     growth_distribution_mean = st.sidebar.number_input('Growth Mean', value=1.5)
     growth_distribution_std = st.sidebar.number_input('Growth Standard Deviation', value=0.5)
 
@@ -99,31 +104,47 @@ def main():
                                          growth_distribution_mean, growth_distribution_std, vc_power_law_exponent)
 
     st.header('Simulation Results')
+    st.subheader('Raw Data')
+    st.dataframe(df)
     st.subheader('Summary Statistics')
-    st.dataframe(summary[['growth_deals', 'mean_return']])
+    st.dataframe(summary)
 
-    # Combined probability distribution per scenario
-    st.header('Portfolio Return Distribution per Scenario (Combined)')
-    fig, ax = plt.subplots()
-    for i, row in summary.iterrows():
-        sns.histplot(row['return_distribution'], kde=True, ax=ax, bins=np.linspace(0, 20, 50), label=f"{row['growth_deals']} Growth Deals", stat="probability")
-    ax.set_xlabel('Return (Multiples of Fund)')
-    ax.set_ylabel('Probability')
-    ax.legend()
-    st.pyplot(fig)
+    st.header('Portfolio Return Distribution')
 
-    # Probability distribution of expected return from just growth deals vs VC deals
-    st.header('Probability Distribution of Expected Return (Growth vs VC Deals)')
-    vc_returns = np.concatenate(df['vc_returns'].values) / n_investments
-    growth_returns = np.concatenate(df['growth_returns'].values) / n_investments
-    fig, ax = plt.subplots()
-    sns.histplot(vc_returns, kde=True, color='blue', label='VC Deals', ax=ax, bins=np.linspace(0, 200, 50), stat="probability")
-    sns.histplot(growth_returns, kde=True, color='green', label='Growth Deals', ax=ax, bins=np.linspace(0, 20, 50), stat="probability")
-    ax.set_xlabel('Return (Multiples of Investment)')
-    ax.set_ylabel('Probability')
-    ax.legend()
-    st.pyplot(fig)
+    st.subheader('VC Deals')
+    vc_chart_data = np.concatenate(df['vc_returns'].values)
+    fig_vc, ax_vc = plt.subplots()
+    sns.histplot(vc_chart_data, kde=True, ax=ax_vc, stat="probability")
+    ax_vc.set_xlabel('Return')
+    ax_vc.set_ylabel('Probability')
+    st.pyplot(fig_vc)
+
+    st.subheader('Growth Deals')
+    growth_chart_data = np.concatenate(df['growth_returns'].values)
+    fig_growth, ax_growth = plt.subplots()
+    sns.histplot(growth_chart_data, kde=True, ax=ax_growth, stat="probability")
+    ax_growth.set_xlabel('Return')
+    ax_growth.set_ylabel('Probability')
+    st.pyplot(fig_growth)
+
+    st.subheader('Combined')
+    fig_combined, ax_combined = plt.subplots()
+    sns.histplot(vc_chart_data, kde=True, color='blue', label='VC Deals', ax=ax_combined, stat="probability")
+    sns.histplot(growth_chart_data, kde=True, color='red', label='Growth Deals', ax=ax_combined, stat="probability")
+    ax_combined.legend()
+    ax_combined.set_xlabel('Return')
+    ax_combined.set_ylabel('Probability')
+    st.pyplot(fig_combined)
+
+    st.header('Portfolio Return vs Number of Growth Deals')
+    fig2, ax2 = plt.subplots()
+    sns.lineplot(x='growth_deals', y='mean_return', data=summary, ax=ax2, label='Mean Return')
+    ax2.fill_between(summary['growth_deals'], summary['percentile_25'], summary['percentile_75'], alpha=0.2)
+    ax2.legend()
+    ax2.set_xlabel('Number of Growth Deals')
+    ax2.set_ylabel('Portfolio Return')
+    st.pyplot(fig2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
