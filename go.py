f5458ba3a225b44d63097f84179fc26cc01324c5
tokalopaidi1 <129062153+tokalopaidi1@users.noreply.github.com>
2023-06-20 12:00:46 +0300

    Update go.py
    Changed some default values

--- go.py
+++ go.py
@@ -47,21 +47,21 @@ def main():
     st.title("Monte Carlo Simulation for Portfolio Returns")
 
     # Sidebar controls
-    n_runs = st.sidebar.number_input("Number of simulations:", min_value=100, value=1000, step=100)
-    fund = st.sidebar.number_input("Initial Fund:", min_value=100000, value=100000000, step=100000)
+    n_runs = st.sidebar.number_input("Number of simulations:", min_value=10, value=100, step=10)
+    fund = st.sidebar.number_input("Initial Fund:", min_value=100000, value=8000000, step=100000)
     n_investments = st.sidebar.number_input("Number of Investments:", min_value=1, value=20, step=1)
     
     st.sidebar.subheader("VC Investments")
     vc_failure_rate = st.sidebar.slider("VC Failure Rate:", min_value=0.0, max_value=1.0, value=0.65, step=0.01)
     vc_min_return = st.sidebar.number_input("VC Min Return Multiplier:", min_value=1.0, value=1.0, step=0.1)
-    vc_max_return = st.sidebar.number_input("VC Max Return Multiplier:", min_value=1.0, value=25.0, step=0.1)
-    vc_power_law_exponent = st.sidebar.slider("VC Power Law Exponent:", min_value=0.0, max_value=5.0, value=2.0, step=0.1)
+    vc_max_return = st.sidebar.number_input("VC Max Return Multiplier:", min_value=1.0, value=200.0, step=0.1)
+    vc_power_law_exponent = st.sidebar.slider("VC Power Law Exponent:", min_value=0.0, max_value=10.0, value=2.0, step=0.1)
     
     st.sidebar.subheader("Growth Investments")
     growth_failure_rate = st.sidebar.slider("Growth Failure Rate:", min_value=0.0, max_value=1.0, value=0.2, step=0.01)
-    growth_min_return = st.sidebar.number_input("Growth Min Return Multiplier:", min_value=1.0, value=3.0, step=0.1)
+    growth_min_return = st.sidebar.number_input("Growth Min Return Multiplier:", min_value=1.0, value=1.0, step=0.1)
     growth_max_return = st.sidebar.number_input("Growth Max Return Multiplier:", min_value=1.0, value=30.0, step=0.1)
-    growth_distribution_mean = st.sidebar.slider("Growth Distribution Mean:", min_value=0.0, max_value=50.0, value=15.0, step=1.0)
+    growth_distribution_mean = st.sidebar.slider("Growth Distribution Mean:", min_value=0.0, max_value=50.0, value=5.0, step=1.0)
     growth_distribution_std = st.sidebar.slider("Growth Distribution Std Dev:", min_value=1.0, max_value=20.0, value=7.0, step=1.0)
     growth_skewness = st.sidebar.slider("Growth Distribution Skewness:", min_value=0.0, max_value=10.0, value=4.0, step=0.1)
     
