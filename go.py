d2c861ab47e9dd19cd9851d8de701d948b3432de
tokalopaidi1 <129062153+tokalopaidi1@users.noreply.github.com>
2023-06-20 12:29:03 +0300

    Update go.py
    In the monte_carlo_simulation function, I changed the way the growth investments are generated. It uses a skewed normal distribution, and I scaled the values to match the user's input range for minimum and maximum returns.
    In the Histogram, I have added the KDE (Kernel Density Estimate) plots to show the curves for both VC and Growth deals.
    I've included summary statistics and raw data tables at the end of the code.

--- go.py
+++ go.py
@@ -3,10 +3,10 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy.stats import powerlaw, skewnorm
+from scipy.stats import skewnorm, powerlaw
 
 
-@st.cache
+@st.cache(suppress_st_warning=True)
 def monte_carlo_simulation(n_runs, fund, n_investments, vc_failure_rate, vc_min_return, vc_max_return, vc_power_law_exponent,
                            growth_failure_rate, growth_min_return, growth_max_return, growth_distribution_mean, growth_distribution_std, growth_skewness):
 
@@ -22,19 +22,18 @@ def monte_carlo_simulation(n_runs, fund, n_investments, vc_failure_rate, vc_min_
                 else:
                     multiplier = max(powerlaw.rvs(a=vc_power_law_exponent, scale=1.0), 1.0)
                     vc_investments.append(np.random.uniform(vc_min_return, vc_max_return) * multiplier)
-                    
+            
             growth_investments = []
             for _ in range(n_growth):
                 p = np.random.rand()
                 if p < growth_failure_rate:
                     growth_investments.append(0)
                 else:
-                    # Generate a value from the skewed normal distribution
-                    investment = skewnorm.rvs(a=growth_skewness, loc=growth_distribution_mean, scale=growth_distribution_std)
-                    # Clip the investment returns to the specified range
-                    investment = np.clip(investment, growth_min_return, growth_max_return)
-                    growth_investments.append(investment)
-
+                    skewed_normal_value = skewnorm.rvs(growth_skewness, size=1)[0]
+                    normalized_value = (skewed_normal_value - skewed_normal_value.min()) / (skewed_normal_value.max() - skewed_normal_value.min())
+                    scaled_value = normalized_value * (growth_max_return - growth_min_return) + growth_min_return
+                    growth_investments.append(scaled_value)
+            
             total_roi = sum(vc_investments) + sum(growth_investments)
             data.append([n_growth, total_roi])
 
@@ -68,38 +67,32 @@ def main():
     growth_max_return = st.sidebar.number_input("Growth Max Return Multiplier:", min_value=1.0, value=30.0, step=0.1)
     growth_distribution_mean = st.sidebar.slider("Growth Distribution Mean:", min_value=0.0, max_value=50.0, value=15.0, step=1.0)
     growth_distribution_std = st.sidebar.slider("Growth Distribution Std Dev:", min_value=1.0, max_value=20.0, value=7.0, step=1.0)
-    growth_skewness = st.sidebar.slider("Growth Distribution Skewness:", min_value=-10.0, max_value=10.0, value=4.0, step=0.1)
+    growth_skewness = st.sidebar.slider("Growth Distribution Skewness:", min_value=0.0, max_value=10.0, value=4.0, step=0.1)
     
     data, summary = monte_carlo_simulation(n_runs, fund, n_investments,
                                            vc_failure_rate, vc_min_return, vc_max_return, vc_power_law_exponent,
                                            growth_failure_rate, growth_min_return, growth_max_return,
                                            growth_distribution_mean, growth_distribution_std, growth_skewness)
 
-    # Plotting
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax.plot(summary.growth_deals, summary.mean_return, label='Mean ROI', color='blue')
-    ax.plot(summary.growth_deals, summary.percentile_25, label='25th Percentile', color='red')
-    ax.plot(summary.growth_deals, summary.percentile_75, label='75th Percentile', color='green')
-    ax.axhline(y=fund * 2, color='gray', linestyle='dashed')
-    ax.axhline(y=fund * 3, color='gray', linestyle='dashed')
-    ax.axhline(y=fund * 5, color='green', linestyle='dashed')
-    ax.set_title('Monte Carlo Simulation of Portfolio Returns')
-    ax.set_xlabel('Number of Growth Investments')
-    ax.set_ylabel('Mean Return on Investment')
-    ax.legend(['Mean ROI', '25th Percentile', '75th Percentile', '2x Fund', '3x Fund', '5x Fund'])
-    st.pyplot(fig)
-
     # Histogram
     fig2, ax2 = plt.subplots()
     vc_only_data = data[data['growth_deals'] == 0]['roi']
     growth_only_data = data[data['growth_deals'] == n_investments]['roi']
-    sns.histplot(vc_only_data, ax=ax2, color='blue', label='VC Deals Only', bins=50)
-    sns.histplot(growth_only_data, ax=ax2, color='red', label='Growth Deals Only', bins=50)
+    sns.histplot(vc_only_data, ax=ax2, color='blue', label='VC Deals Only', bins=50, kde=True)
+    sns.histplot(growth_only_data, ax=ax2, color='red', label='Growth Deals Only', bins=50, kde=True)
     ax2.set_title('Histogram of Returns for VC Deals Only vs Growth Deals Only')
     ax2.set_xlabel('Return on Investment')
     ax2.set_ylabel('Frequency')
     ax2.legend()
     st.pyplot(fig2)
+
+    # Summary statistics
+    st.subheader("Summary Statistics")
+    st.table(summary)
+
+    # Raw data
+    st.subheader("Raw Data")
+    st.write(data)
 
 
 if __name__ == "__main__":
