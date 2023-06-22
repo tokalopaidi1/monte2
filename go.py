bb88a456bf766cf09b31325af2aa00731d1f0d9e
tokalopaidi1 <129062153+tokalopaidi1@users.noreply.github.com>
2023-06-22 22:29:39 +0300

    Update go.py
    I have modified the way growth investments are simulated, by sampling from a normal distribution for the multiplier and ensuring that it's within the minimum and maximum multiplier values. This should give you a more realistic representation of growth investments. You can experiment with different values for the mean and standard deviation of the multiplier distribution to see how they affect the simulation results.

--- go.py
+++ go.py
@@ -22,16 +22,17 @@ def monte_carlo_simulation(n_runs, fund, n_investments, vc_failure_rate, vc_min_
                 else:
                     multiplier = max(powerlaw.rvs(a=vc_power_law_exponent, scale=1.0), 1.0)
                     vc_investments.append(np.random.uniform(vc_min_return, vc_max_return) * multiplier)
-                    
+
             growth_investments = []
             for _ in range(n_growth):
                 p = np.random.rand()
                 if p < growth_failure_rate:
                     growth_investments.append(0)
                 else:
-                    a, b = (growth_min_return - growth_distribution_mean) / growth_distribution_std, (growth_max_return - growth_distribution_mean) / growth_distribution_std
-                    growth_investments.append(truncnorm.rvs(a, b, loc=growth_distribution_mean, scale=growth_distribution_std))
-            
+                    multiplier = np.random.normal(growth_distribution_mean, growth_distribution_std)
+                    multiplier = max(min(multiplier, growth_max_return), growth_min_return)
+                    growth_investments.append(multiplier)
+
             total_roi = sum(vc_investments) + sum(growth_investments)
             data.append([n_growth, total_roi])
 
@@ -52,20 +53,20 @@ def main():
     n_runs = st.sidebar.number_input("Number of simulations:", min_value=100, value=1000, step=100)
     fund = st.sidebar.number_input("Initial Fund:", min_value=100000, value=100000000, step=100000)
     n_investments = st.sidebar.number_input("Number of Investments:", min_value=1, value=20, step=1)
-    
+
     st.sidebar.subheader("VC Investments")
     vc_failure_rate = st.sidebar.slider("VC Failure Rate:", min_value=0.0, max_value=1.0, value=0.65, step=0.01)
     vc_min_return = st.sidebar.number_input("VC Min Return Multiplier:", min_value=1.0, value=1.0, step=0.1)
     vc_max_return = st.sidebar.number_input("VC Max Return Multiplier:", min_value=1.0, value=25.0, step=0.1)
     vc_power_law_exponent = st.sidebar.slider("VC Power Law Exponent:", min_value=0.0, max_value=5.0, value=2.0, step=0.1)
-    
+
     st.sidebar.subheader("Growth Investments")
     growth_failure_rate = st.sidebar.slider("Growth Failure Rate:", min_value=0.0, max_value=1.0, value=0.2, step=0.01)
     growth_min_return = st.sidebar.number_input("Growth Min Return Multiplier:", min_value=1.0, value=3.0, step=0.1)
     growth_max_return = st.sidebar.number_input("Growth Max Return Multiplier:", min_value=1.0, value=30.0, step=0.1)
     growth_distribution_mean = st.sidebar.slider("Growth Distribution Mean:", min_value=0.0, max_value=50.0, value=15.0, step=1.0)
     growth_distribution_std = st.sidebar.slider("Growth Distribution Std Dev:", min_value=1.0, max_value=20.0, value=7.0, step=1.0)
-    
+
     data, summary = monte_carlo_simulation(n_runs, fund, n_investments,
                                            vc_failure_rate, vc_min_return, vc_max_return, vc_power_law_exponent,
                                            growth_failure_rate, growth_min_return, growth_max_return,
