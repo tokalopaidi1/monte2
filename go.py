45ff9567e6cb03165fc348e5dd9d7a3c542aede1
tokalopaidi1 <129062153+tokalopaidi1@users.noreply.github.com>
2023-06-23 02:06:09 +0300

    Update go.py

--- go.py
+++ go.py
@@ -3,79 +3,78 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy.stats import lognorm, powerlaw
 
 
-def vc_monte_carlo_simulation(n_runs, n_vc, vc_failure_rate, vc_min_return, vc_max_return, vc_power_law_exponent):
-    vc_investments = np.zeros(n_runs)
-    for i in range(n_vc):
-        p = np.random.rand(n_runs)
-        multipliers = powerlaw.rvs(a=vc_power_law_exponent, scale=(vc_max_return - vc_min_return), size=n_runs) + vc_min_return
-        vc_investments += np.where(p >= vc_failure_rate, multipliers, 0)
-    return vc_investments
+@st.cache
+def monte_carlo_simulation_vc(n_runs, fund, n_investments, failure_rate, min_return, max_return, power_law_exponent):
+    data = []
+    for _ in range(n_runs):
+        vc_investments = 0
+        for _ in range(n_investments):
+            p = np.random.rand()
+            if p >= failure_rate:
+                multiplier = max(np.random.power(a=power_law_exponent), 1.0)
+                vc_investments += np.random.uniform(min_return, max_return) * multiplier
+        total_roi = vc_investments
+        data.append(total_roi)
+
+    return pd.Series(data) * fund / n_investments
 
 
-def growth_monte_carlo_simulation(n_runs, n_growth, growth_failure_rate, growth_distribution_mean, growth_distribution_std):
-    growth_investments = np.zeros(n_runs)
-    for i in range(n_growth):
-        p = np.random.rand(n_runs)
-        multipliers = lognorm.rvs(s=growth_distribution_std, scale=np.exp(growth_distribution_mean), size=n_runs)
-        growth_investments += np.where(p >= growth_failure_rate, multipliers, 0)
-    return growth_investments
+@st.cache
+def monte_carlo_simulation_growth(n_runs, fund, n_investments, failure_rate, lognorm_mean, lognorm_std):
+    data = []
+    for _ in range(n_runs):
+        growth_investments = 0
+        for _ in range(n_investments):
+            p = np.random.rand()
+            if p >= failure_rate:
+                growth_investments += np.random.lognormal(mean=lognorm_mean, sigma=lognorm_std)
+        total_roi = growth_investments
+        data.append(total_roi)
+
+    return pd.Series(data) * fund / n_investments
 
 
 def main():
     st.title("Monte Carlo Simulation for Portfolio Returns")
 
     # Sidebar controls
-    n_runs = st.sidebar.number_input("Number of simulations:", min_value=100, value=1000, step=10)
-    fund = st.sidebar.number_input("Initial Fund:", min_value=100000, value=10000000, step=100000)
-    n_investments = st.sidebar.number_input("Number of Investments:", min_value=1, value=20, step=1)
-
+    n_runs = st.sidebar.number_input("Number of simulations:", min_value=100, value=1000, step=100)
+    fund = st.sidebar.number_input("Initial Fund:", min_value=100000, value=100000000, step=100000)
+    
     st.sidebar.subheader("VC Investments")
+    n_investments_vc = st.sidebar.number_input("Number of VC Investments:", min_value=1, value=10, step=1)
     vc_failure_rate = st.sidebar.slider("VC Failure Rate:", min_value=0.0, max_value=1.0, value=0.65, step=0.01)
     vc_min_return = st.sidebar.number_input("VC Min Return Multiplier:", min_value=1.0, value=1.0, step=0.1)
-    vc_max_return = st.sidebar.number_input("VC Max Return Multiplier:", min_value=1.0, value=200.0, step=0.1)
-    vc_power_law_exponent = st.sidebar.slider("VC Power Law Exponent:", min_value=0.0, max_value=10.0, value=2.0, step=0.01)
+    vc_max_return = st.sidebar.number_input("VC Max Return Multiplier:", min_value=1.0, value=25.0, step=0.1)
+    vc_power_law_exponent = st.sidebar.slider("VC Power Law Exponent:", min_value=0.0, max_value=5.0, value=2.0, step=0.1)
 
     st.sidebar.subheader("Growth Investments")
-    growth_failure_rate = st.sidebar.slider("Growth Failure Rate:", min_value=0.0, max_value=1.0, value=0.75, step=0.01)
-    growth_distribution_mean = st.sidebar.slider("Growth Distribution Mean:", min_value=0.0, max_value=50.0, value=5, step=0.1)
-    growth_distribution_std = st.sidebar.slider("Growth Distribution Std Dev:", min_value=0.1, max_value=20.0, value=4.0, step=0.01)
+    n_investments_growth = st.sidebar.number_input("Number of Growth Investments:", min_value=1, value=10, step=1)
+    growth_failure_rate = st.sidebar.slider("Growth Failure Rate:", min_value=0.0, max_value=1.0, value=0.2, step=0.01)
+    growth_lognorm_mean = st.sidebar.slider("Growth Log-Normal Mean (μ of log):", min_value=0.0, max_value=5.0, value=2.0, step=0.1)
+    growth_lognorm_std = st.sidebar.slider("Growth Log-Normal Std Dev (σ of log):", min_value=0.1, max_value=5.0, value=0.5, step=0.1)
+    
+    vc_data = monte_carlo_simulation_vc(n_runs, fund, n_investments_vc, vc_failure_rate, vc_min_return, vc_max_return, vc_power_law_exponent)
+    growth_data = monte_carlo_simulation_growth(n_runs, fund, n_investments_growth, growth_failure_rate, growth_lognorm_mean, growth_lognorm_std)
+    
+    # Histogram
+    fig, ax = plt.subplots()
+    sns.histplot(vc_data, bins=50, color='blue', label='VC Deals', ax=ax, stat='density', kde=True)
+    sns.histplot(growth_data, bins=50, color='green', label='Growth Deals', ax=ax, stat='density', kde=True)
+    ax.set_xlabel('TVPI')
+    ax.set_ylabel('Density')
+    ax.legend()
+    st.pyplot(fig)
 
-    data = []
-    for n_growth in range(n_investments + 1):
-        n_vc = n_investments - n_growth
-        vc_returns = vc_monte_carlo_simulation(n_runs, n_vc, vc_failure_rate, vc_min_return, vc_max_return, vc_power_law_exponent)
-        growth_returns = growth_monte_carlo_simulation(n_runs, n_growth, growth_failure_rate, growth_distribution_mean, growth_distribution_std)
+    # Summary statistics
+    st.subheader("Summary Statistics")
+    st.subheader("VC Deals")
+    st.table(vc_data.describe())
 
-        combined_returns = vc_returns + growth_returns
-        combined_returns *= fund / n_investments
-        mean_return = np.mean(combined_returns)
-        data.append([n_vc, n_growth, mean_return])
-
-    # Creating a DataFrame
-    df = pd.DataFrame(data, columns=["VC Investments", "Growth Investments", "Mean Return"])
-
-    # Creating a Histogram
-    fig1, ax1 = plt.subplots(figsize=(10, 5))
-    sns.histplot(combined_returns, kde=True, ax=ax1, stat='density')
-    ax1.set_xlabel("Portfolio Return")
-    ax1.set_ylabel("Probability Density")
-    ax1.set_title("Portfolio Return Distribution")
-
-    # Creating a Possibility vs LTVI Plot
-    fig2, ax2 = plt.subplots(figsize=(10, 5))
-    ax2.plot(df['VC Investments'], df['Mean Return'], label='VC Investments')
-    ax2.plot(df['Growth Investments'], df['Mean Return'], label='Growth Investments')
-    ax2.set_xlabel("Number of Investments")
-    ax2.set_ylabel("Mean Return")
-    ax2.legend()
-    ax2.set_title("Possibility vs LTVI")
-
-    st.table(df)
-    st.pyplot(fig1)
-    st.pyplot(fig2)
+    st.subheader("Growth Deals")
+    st.table(growth_data.describe())
 
 
 if __name__ == "__main__":
